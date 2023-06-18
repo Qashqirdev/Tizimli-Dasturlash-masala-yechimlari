26061c8fbee367f564126153c92d76fed2897609
Laziz Jamoliddinov <135235031+Qashqirdev@users.noreply.github.com>
2023-06-18 11:36:49 +0500

    Add files via upload

---
+++ teskari.cpp
@@ -0,0 +1,13 @@
+// Kiritilgan satrni teskari tartibda chiqaruvchi programma tuzilsin 
+#include <bits/stdc++.h>
+using namespace std;
+int main(void)
+{
+  string s;
+  cin >> s;
+    int len = s.length();
+  int n = len;
+  while(n--)
+    cout << s[n];
+  return (0);
+}
